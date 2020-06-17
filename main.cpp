22a0d90376dbe5b2646ffb0cfae0f769f0a213a9
ardeshiri <ardeshiri.arash@gmail.com>
2020-06-17 14:06:04 +0430

    minor changes 2

--- main.cpp
+++ main.cpp
@@ -27,27 +27,20 @@ void f3()
 int main()
 {
    dd::Thread_p<3> th{};
-    // packaged_task<void()> pt1{f1};
-    // th.push_task(move(pt1), 0);
+
    for(int i = 0 ; i < 100; i++)
    {
-      packaged_task<void()> pt2{f1};
-      packaged_task<void()> pt3{f2};
-      packaged_task<void()> pt5{f3};
-     // packaged_task<void()> pt6{f3};
-
+      packaged_task<void()> pt1{f1};
+      packaged_task<void()> pt2{f2};
+      packaged_task<void()> pt3{f3};
+      th.push_task(move(pt1), 10);
       th.push_task(move(pt2), 10);
       th.push_task(move(pt3), 10);
-      th.push_task(move(pt5), 10);
-     // if(i == 80) th.stop(2);
-     // th.push_task(move(pt6), 10);
    }
-
    packaged_task<void()> ptx{f1};
-   auto du = ptx.get_future();
+   auto fut = ptx.get_future();
    th.push_task(move(ptx), 0);
-   du.get();
-    // this_thread::sleep_for(4s);
+   fut.get();
 
    return 0;
 }
